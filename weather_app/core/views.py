2199ace9445ca86989b061ce6ade24915c179434
thekkr <kiranreddy6362@gmail.com>
2021-07-29 10:25:23 +0530

    core views and template

--- weather_app/core/views.py
+++ weather_app/core/views.py
@@ -1,3 +1,13 @@
 from flask import render_template,Blueprint,requests
 
 core = Blueprint('core',__name__)
+
+
+@core.route('/')
+def index():
+    return render_template("index.html")
+
+
+@core.route('/info')
+def info():
+    return render_template("info.html")
